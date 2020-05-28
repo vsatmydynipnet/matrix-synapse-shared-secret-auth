d9d3f94935be128bef31634a857b756c819813b6
KMJ <vs@mydynip.net>
2020-05-28 13:32:37 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="shared_secret_authenticator",
-    version="0.0.1",
+    version="2.0.1",
     py_modules=['shared_secret_authenticator'],
-    description="Shared Secret Authenticator password provider module for Matrix Synapse",
+    description="Shared Secret Authenticator password provider module for Matrix Synapse. Fork by KMJ for User Restrictions",
     include_package_data=True,
     zip_safe=True,
     install_requires=[],
